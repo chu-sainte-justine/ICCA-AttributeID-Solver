cbff13fb82f70434afba7c3ff1aa14352f052059
Cescollino <67806795+Cescollino@users.noreply.github.com>
2024-04-03 13:39:16 -0400

    Update create_conda_env.sh

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -3,7 +3,7 @@
 # Define environment name
 ENV_NAME="ICCA_AttributeID_Solver"
 
-# Create the Conda environment with required packages
-conda create --name "$ENV_NAME" pandas sqlalchemy
+# Create the Conda environment with packages listed in requirements.txt
+conda create --name "$ENV_NAME" --file requirements.txt
 
-echo "Conda environment '$ENV_NAME' created with required packages."
+echo "Conda environment '$ENV_NAME' created with packages from requirements.txt."
